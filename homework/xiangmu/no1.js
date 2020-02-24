c1a0befb536471726f211c9d9f89972e608608ab
dongjiahui1 <960995259@qq.com>
2020-02-24 12:41:26 +0800

    Signed-off-by: dongjiahui1 <960995259@qq.com>

---
+++ homework/xiangmu/no1.js
@@ -0,0 +1,77 @@
+Vue.component('no1', {
+    template:`<div>
+    <div style="width: 80% ;margin: 0 auto;background:#f0f0f0;min-height: 700px;position: relative;">
+                <div style="">
+                    <div style="text-align: center;width: 80px;margin: 0 auto;padding-top: 70px">
+                        <h4 style="width: 80px;height: 31px;font-size: 20px;text-align: center;"><a href="#/no3"
+                                style="color: #c6c6c6">主推产品</a></h4>
+                        <span
+                            style="width: 80px;height: 53px;font: size 15px;padding:0 0 40px;text-align: center;font-size: 12px"><a
+                                href="#/no3" style="color: #c6c6c6">PRODUCT</a></span>
+                    </div>
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide"><img
+                                    src="https://ccdn.goodq.top/caches/40c05a76d970c388d38b924930a6f580/aHR0cHM6Ly81N2E3ZTZhYWYyZjc4LnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDkvZmVkNjAyMDAyZTZiNjRkYjVhZDg0MmEwN2U5Yjg5ODYucG5n.png"
+                                    alt=""></div>
+                            <div class="swiper-slide"><img
+                                    src="https://ccdn.goodq.top/caches/40c05a76d970c388d38b924930a6f580/aHR0cHM6Ly81N2E3ZTZhYWYyZjc4LnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDkvODBkMjY5NjYzNDJmNTM1ZjdkYmEwMjY2OTNmMmQ5ZjcucG5n.png"
+                                    alt=""></div>
+                            <div class="swiper-slide"><img
+                                    src="https://ccdn.goodq.top/caches/40c05a76d970c388d38b924930a6f580/aHR0cHM6Ly81N2E3ZTZhYWYyZjc4LnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDkvNzY5MDExMmQ0MjIzN2Y5YTg0OWVjNjc0N2NmOGMxYWYucG5n.png"
+                                    alt=""></div>
+                        </div>
+                        <!-- 如果需要分页器 -->
+                        <div class="swiper-pagination"></div>
+                    </div>
+                    <div style="padding-bottom: 30px;">
+                        <div style="text-align: center">
+                            <span style="font-family: arial"><span style="font-size: 68px">U</span><span
+                                    style="font-size: 40px"><span style="font-size: 68px;">m</span><span
+                                        style="font-size: 26px">360</span></span></span>
+                        </div>
+                        <div style="text-align: center">
+                            <span style="font-size: 16px;color:#393939;font-family: 微软雅黑;font-weight: bold;">精细快速低剂量成像,高效安全扫描体验</span>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div style="width: 60%;height: 170px;background: chartreuse;margin: 0 auto;position:absolute;bottom: -98px;left: 20%;box-shadow: 10px 0px 10px -10px, -10px 0px 10px -10px #000000 ">
+                    <div style="width: 100px;height:50px;text-align: center;margin-top: 66px;margin-left: 40px;border-right: 1px solid #cccccc;float: left;">
+                        <div style="width: 60px;height:20px;color: #aaaaaa;font-size: 20px;padding: 0 0 5px">
+                            12000+
+                        </div>
+                        <div style="font-size: 14px;width:60px;height:19px;">
+                            成功案例
+                        </div>
+                    </div>
+                    <div style="width: 100px;height:50px;text-align: center;margin-top: 66px;margin-left: 40px;border-right: 1px solid #cccccc;float: left;">
+                        <div style="width: 60px;height:20px;color: #aaaaaa;font-size: 20px;padding: 0 0 5px">
+                            010+
+                        </div>
+                        <div style="font-size: 14px;width:60px;height:19px;">
+                            解决方案
+                        </div>
+                    </div>
+                    <div style="width: 100px;height:50px;text-align: center;margin-top: 66px;margin-left: 40px;border-right: 1px solid #cccccc;float: left;">
+                        <div style="width: 60px;height:20px;color: #aaaaaa;font-size: 20px;padding: 0 0 5px">
+                            200+
+                        </div>
+                        <div style="font-size: 14px;width:60px;height:19px;">
+                            合作伙伴
+                        </div>
+                    </div>
+                    <div style="width: 100px;height:50px;text-align: center;margin-top: 66px;margin-left: 40px;float: left;">
+                        <div style="width: 60px;height:20px;color: #aaaaaa;font-size: 20px;padding: 0 0 5px">
+                            050
+                        </div>
+                        <div style="font-size: 14px;width:60px;height:19px;">
+                            技术专利
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+    
+    </div>`
+})
